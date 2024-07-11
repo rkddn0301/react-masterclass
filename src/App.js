4f0494ae033ea23881f019688b7ea09d0fb684d1
rkddn0301 <rkddn0301@naver.com>
2024-07-11 17:10:06 +0900

    2.2 Adapting and Extending 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ const Father = styled.div`
   display: flex;
 `;
 
+// props를 생성하여 배경색 지정을 return에서 할 수 있게 전달할 수 있다.
 const Box = styled.div`
-  background-color: teal;
-  width: 100;
-  height: 100;
+  background-color: ${(props) => props.bgcolor};
+  width: 100px;
+  height: 100px;
+`;
+
+// Box styled를 그대로 가져와 원형이라는 추가 속성만 만들 수 있다.
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <Box />
-      <Box />
+      <Box bgcolor="teal" />
+      <Circle bgcolor="tomato" />
     </Father>
   );
 }
