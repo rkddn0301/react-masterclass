488e1a3c851ba6a7a6c8c026437d14993f115799
rkddn0301 <rkddn0301@naver.com>
2024-07-26 19:10:09 +0900

    8.0 Introduction

--- src/App.tsx
+++ src/App.tsx
@@ -3,73 +3,27 @@ import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 import { darkTheme } from "./theme";
 import ToDoList from "./ToDoList";
 
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
-  html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
-}
-body {
-	line-height: 1;
-}
-ol, ul {
-	list-style: none;
-}
-blockquote, q {
-	quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-}
-body {
-  font-family: 'Source Sans Pro', sans-serif;
-  background-color: ${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor}
-}
-a {
-  text-decoration: none;
-  color: inherit; // 부모 요소의 텍스트 색상을 상속받음
-}
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        <ToDoList />
-      </ThemeProvider>
-    </>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,4 @@
 import { DefaultTheme } from "styled-components";
 
 // 컬러 출처 : https://flatuicolors.com/palette/gb
-export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#4cd137",
-  cardBgColor: "transparent",
-};
+export const darkTheme: DefaultTheme = {};
