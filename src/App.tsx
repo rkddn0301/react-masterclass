8f413c1a8aed0557551193eb2b5b197cb27e70fc
rkddn0301 <rkddn0301@naver.com>
2024-07-30 18:56:58 +0900

    9.2 Header part Two

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import Tv from "./Routes/Tv";
 import Search from "./Routes/Search";
 import Header from "./Components/Header";
 
+// path가 /인 Route를 맨 아래에 두는 이유는 Link를 통해 경로에 접속할 때 /는 무조건 포함되기 때문에
+// / 경로 컴포넌트가 먼저 접속되어 방지하기 위함이다.
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
         <Route path="/tv">
           <Tv />
         </Route>
         <Route path="/search">
           <Search />
+        </Route>
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </Router>
