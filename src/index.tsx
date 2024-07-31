34d3c6ad6451c94735aeaf8bdd8eb22cc8bf6d64
rkddn0301 <rkddn0301@naver.com>
2024-07-31 20:10:24 +0900

    9.5 Home Screen part One

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import { theme } from "./theme";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -65,14 +66,18 @@ a {
 }
 `;
 
+const client = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <RecoilRoot>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </RecoilRoot>
 );
