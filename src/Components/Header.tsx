de3df20726ff396c93a02aa162019593489d5335
rkddn0301 <rkddn0301@naver.com>
2024-08-01 21:47:44 +0900

    9.14 Search Redirect

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { motion, useAnimation, useScroll } from "framer-motion";
-import { Link, useRouteMatch } from "react-router-dom";
+import { Link, useHistory, useRouteMatch } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Nav = styled(motion.nav)`
   display: flex;
@@ -50,7 +51,7 @@ const Item = styled.li`
   flex-direction: column;
 `;
 
-const Search = styled.span`
+const Search = styled.form`
   color: white;
   display: flex;
   align-items: center;
@@ -114,6 +115,10 @@ const navVariants = {
   },
 };
 
+interface IForm {
+  keyword: string;
+}
+
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false); // 검색 클릭 여부 state
   const homeMatch = useRouteMatch("/");
@@ -132,7 +137,10 @@ function Header() {
         navAnimation.start("top");
       }
     });
-  }, [scrollY]);
+  }, [scrollY, navAnimation]);
+
+  const history = useHistory();
+  const { register, handleSubmit } = useForm<IForm>();
 
   // 검색 클릭 함수
   // useAnimation에 직접 스타일을 삽입할 때
@@ -147,6 +155,11 @@ function Header() {
       inputAnimation.start({ scaleX: 1 });
     }
     setSearchOpen((prev) => !prev);
+  };
+
+  const onValid = (data: IForm) => {
+    console.log(data);
+    history.push(`/search?keyword=${data.keyword}`);
   };
 
   return (
@@ -179,7 +192,7 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <Search>
+        <Search onSubmit={handleSubmit(onValid)}>
           <motion.svg
             onClick={toggleSearch}
             animate={{ x: searchOpen ? -210 : 0 }} // 검색 클릭 여부에 따라 위치 변경
@@ -196,6 +209,7 @@ function Header() {
           </motion.svg>
 
           <Input
+            {...register("keyword", { required: true, minLength: 2 })}
             initial={{ scaleX: 0 }}
             transition={{ type: "linear" }} // 요소가 부드럽고 일관된 속도로 변화
             animate={inputAnimation} // 검색 클릭 여부에 따라 크기 변경
